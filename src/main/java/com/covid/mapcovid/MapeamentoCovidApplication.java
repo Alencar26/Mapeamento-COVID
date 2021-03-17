1adb84dd76c795fffbcf1124836d1189793d40b9
Andre Alencar <aaugusto310@gmail.com>
2021-03-16 21:56:40 -0300

    criação do projeto.

---
+++ src/main/java/com/covid/mapcovid/MapeamentoCovidApplication.java
@@ -0,0 +1,13 @@
+package com.covid.mapcovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeamentoCovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeamentoCovidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covid/mapcovid/MapeamentoCovidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid.mapcovid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapeamentoCovidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
