775d93bd2b917c2ce25b6bc5f497016d90054f45
Julia Louback Ribeiro <julialoubackribeiro2001@gmail.com>
2021-03-29 20:04:57 -0300

    Alterações Mapa

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,5 +1,106 @@
 
-jQuery(document).ready(function(){
-jQuery("#mapsvg").mapSvg({width: 355.74203,height: 588.58795,colors: {
-	baseDefault: "#000000",background: "#000000",selected: 40,hover: 20,directory: "#fafafa",status: {},base: "#25011e"},viewBox: [0,0,355.74203,588.58795],cursor: "pointer",gauge: {on: false,labels: {low: "low",high: "high"},colors: {lowRGB: {r: 85,g: 0,b: 0,a: 1},highRGB: {r: 238,g: 0,b: 0,a: 1},low: "#550000",high: "#ee0000",diffRGB: {r: 153,g: 0,b: 0,a: 0}},min: 0,max: false},source: "/mapsvg/maps/not-calibrated/south-america.svg",title: "South-america",responsive: true});
-});
+
+$(document).ready(function(){
+	
+	var url = window.location;
+	
+	var objPais = [
+		{
+			id: "Argentina",
+			name: "Argentina"
+		}, 
+		{
+			id: "Bolivia",
+			name: "Bolívia"
+		},
+		{
+			id: "Brazil",
+			name: "Brasil"
+		},
+		{
+			id: "Chile",
+			name: "Chile"
+		},
+		{
+			id: "Colombia",
+			name: "Colômbia"
+		},
+		{
+			id: "Ecuador",
+			name: "Equador"
+		},
+		{
+			id: "FrenchGuiana",
+			name: "Guiana Francesa"
+		},
+			{
+			id: "Guyana",
+			name: "Guiana"
+		},
+			{
+			id: "Peru",
+			name: "Peru"
+		},
+		{
+			id: "Paraguay",
+			name: "Paraguai"
+		},
+		{
+			id: "Suriname",
+			name: "Suriname"
+		},
+		{
+			id: "Uruguay",
+			name: "Uruguai"
+		},
+		{
+			id: "Venezuela",
+			name: "Venezuela"
+		}
+	]
+	
+	
+    $("#Argentina, #Bolivia, #Brazil, #Chile, #Colombia, #Ecuador, #FrenchGuiana, #Guyana, #Peru, #Paraguay, #Suriname, #Uruguay, #Venezuela").click(function(event) {
+		ajaxPost(event);
+	});
+	
+	
+	function ajaxPost(event){
+	
+		var formData = {}
+		
+		if(event.target.id == "FrenchGuiana"){
+			formData = {name: 'French%20Guiana'}
+		} else {
+			formData = {name: event.target.id}
+		}
+		
+		var nomeObj = objPais.find(element => element.id == event.target.id);
+		
+		$.ajax({
+				type : "POST",
+				contentType : "application/json",
+				url : url + "countryJSON",
+				data : JSON.stringify(formData),
+				dataType : "json",
+				success : function(result) {
+					$("#totalCasos").html(result.cases.toLocaleString());
+					$("#totalAtivos").html(result.active.toLocaleString());
+					$("#totalObitos").html(result.deaths.toLocaleString());
+					$("#totalRecuperados").html(result.recovered.toLocaleString());
+					$("#nomePais").html(nomeObj.name);
+					$("#bandeira").attr('src',result.countryInfo.flag);
+					$("#populacao").html(result.population.toLocaleString());
+					
+					
+					console.log(result.countryInfo.flag)
+					console.log(result)
+				},
+				error : function(e) {
+					alert("Error!")
+					console.log("ERROR: ", e);
+				}
+			});
+	}
+
+})
