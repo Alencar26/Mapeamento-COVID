bc442b685209c28d7515b957eb40e84b684e1ee4
Julia Louback Ribeiro <julialoubackribeiro2001@gmail.com>
2021-04-01 05:51:22 -0300

    Update WebController

--- src/main/java/com/covid/mapcovid/Controller/WebController.java
+++ src/main/java/com/covid/mapcovid/Controller/WebController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.covid.mapcovid.Model.Entities.Continent;
+import com.covid.mapcovid.Model.Entities.ContinentOperation;
 import com.covid.mapcovid.Model.Entities.Country;
 import com.covid.mapcovid.Model.Entities.CountryOperation;
 import com.covid.mapcovid.Model.Enums.Countries;
@@ -17,25 +19,26 @@ public class WebController {
 	@GetMapping({"/"})
     public String index(Model model)  {
     	
-    	Country countrySelect = CountryOperation.GetCountry("Brazil");
+
+		Continent continentSelect = ContinentOperation.GetContinent("South%20America");
 		
    	 	model.addAttribute("nomePais", "América do Sul");	
-        model.addAttribute("totalCasos", NumberFormat.getIntegerInstance().format(countrySelect.getCases()));
-        model.addAttribute("totalAtivos", NumberFormat.getIntegerInstance().format(countrySelect.getActive()));
-        model.addAttribute("totalObitos", NumberFormat.getIntegerInstance().format(countrySelect.getDeaths()));
-        model.addAttribute("totalRecuperados", NumberFormat.getIntegerInstance().format(countrySelect.getRecovered()));
-        model.addAttribute("totalTestes", NumberFormat.getIntegerInstance().format(countrySelect.getTests()));
-        model.addAttribute("totalCriticos", NumberFormat.getIntegerInstance().format(countrySelect.getCritical()));
-        model.addAttribute("populacao",  NumberFormat.getIntegerInstance().format(countrySelect.getPopulation()));
-        model.addAttribute("hojeCasos", NumberFormat.getIntegerInstance().format(countrySelect.getTodayCases()));
-        model.addAttribute("hojeObitos", NumberFormat.getIntegerInstance().format(countrySelect.getTodayDeaths()));
-        model.addAttribute("hojeRecuperados", NumberFormat.getIntegerInstance().format(countrySelect.getTodayRecovered()));
+        model.addAttribute("totalCasos", NumberFormat.getIntegerInstance().format(continentSelect.getCases()));
+        model.addAttribute("totalAtivos", NumberFormat.getIntegerInstance().format(continentSelect.getActive()));
+        model.addAttribute("totalObitos", NumberFormat.getIntegerInstance().format(continentSelect.getDeaths()));
+        model.addAttribute("totalRecuperados", NumberFormat.getIntegerInstance().format(continentSelect.getRecovered()));
+        model.addAttribute("totalTestes", NumberFormat.getIntegerInstance().format(continentSelect.getTests()));
+        model.addAttribute("totalCriticos", NumberFormat.getIntegerInstance().format(continentSelect.getCritical()));
+        model.addAttribute("populacao",  NumberFormat.getIntegerInstance().format(continentSelect.getPopulation()));
+        model.addAttribute("hojeCasos", NumberFormat.getIntegerInstance().format(continentSelect.getTodayCases()));
+        model.addAttribute("hojeObitos", NumberFormat.getIntegerInstance().format(continentSelect.getTodayDeaths()));
+        model.addAttribute("hojeRecuperados", NumberFormat.getIntegerInstance().format(continentSelect.getTodayRecovered()));
         
-        if(countrySelect.getCasesPerOneMillion() >= 50000){
+        if(continentSelect.getCasesPerOneMillion() >= 50000){
        	 	model.addAttribute("situacao", "Alta");	
        	 	model.addAttribute("cor", "red");	
 
-        } else if(countrySelect.getCasesPerOneMillion() >= 40000){
+        } else if(continentSelect.getCasesPerOneMillion() >= 40000){
        	 	model.addAttribute("situacao", "Constante");
        	 	model.addAttribute("cor", "#F5CA7B");	
 
